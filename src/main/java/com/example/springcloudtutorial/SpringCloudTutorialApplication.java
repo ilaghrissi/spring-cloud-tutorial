26b89bf9987cf19687bb74e570b2748a6c649248
LAGHRISSI Imad <laghrissi.imad@gmail.com>
2024-01-08 23:01:09 +0100

    first commit

---
+++ src/main/java/com/example/springcloudtutorial/SpringCloudTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudTutorialApplication.class, args);
+	}
+
+}
